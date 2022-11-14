acb74a557dde82ac951433ccd38cb2505f514693
victoria husemeyer <victoria_husemeyer@yahoo.com>
2022-11-14 14:23:16 +0000

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root to: "pages#action"
+  # get "contact", to: "pages#contact"
+  get "ask", to: "questions#ask"
 end
